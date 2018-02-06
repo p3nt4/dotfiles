e88eb36585439df05d283cfbec46c86552754ada
p3nt4 <louis.nacfr@gmail.com>
2018-02-06 22:22:39 +1100

    Create lock.sh

---
+++ lock.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+# Take a screenshot
+scrot ~/tmp/screen_locked.png
+
+# Pixellate it 10x
+mogrify -scale 10% -scale 1000% /home/louis/tmp/screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -i ~/tmp/screen_locked.png
+
+# Turn the screen off after a delay.
+
+while pgrep i3lock > /dev/null; do
+	sleep 10;
+	if [ "$(xprintidle)" -gt "10000" ]; 
+	then
+		xset dpms force off;
+	fi
+	if [ "$(xprintidle)" -gt "3600000" ]; 
+	then
+		systemctl suspend;
+	fi
+done
